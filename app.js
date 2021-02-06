97ddeadd1d13794164490cf46054f7c624d645a7
shivanijaiswal123 <shivanij18@navgurukul.org>
2021-02-06 14:32:09 +0530

    updated

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var db = require("./Models/db_connection.js");
+const bodyParser = require("body-parser");
 
 const availability = require("./Controllers/availability.routes");
 
 // Initialize Express App
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
 
 //Base URL
 app.use("/api", availability);
